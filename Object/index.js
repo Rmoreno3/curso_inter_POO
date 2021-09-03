5aba1265d0178d45238fb610def7f9a9c9a96f22
Rmoreno3 <reynaldomoreno173@gmail.com>
2021-09-03 19:40:12 -0400

    Probando los demas metodos del prototipo Object

--- Object/index.js
+++ Object/index.js
@@ -7,14 +7,39 @@ const juan = {
   },
 };
 
-console.log(Object.keys(juan));
-console.log(Object.getOwnPropertyNames(juan));
-console.log(Object.entries(juan));
-console.log(Object.getOwnPropertyDescriptors(juan));
+// console.log(Object.keys(juan));
+// console.log(Object.getOwnPropertyNames(juan));
+// console.log(Object.entries(juan));
 
-Object.defineProperty(juan, "pruebaNASA", {
-  value: "extraterrestre",
-  enumerable: true,
-  writable: true,
-  configurable: true,
-});
+// Object.defineProperty(juan, "navigator", {
+//   value: "Chrome",
+//   enumerable: false,
+//   writable: true,
+//   configurable: true,
+// });
+
+// Object.defineProperty(juan, "editor", {
+//   value: "VSCode",
+//   enumerable: true,
+//   writable: false,
+//   configurable: true,
+// });
+
+// Object.defineProperty(juan, "terminal", {
+//   value: "Bash",
+//   enumerable: true,
+//   writable: false,
+//   configurable: false,
+// });
+
+// Object.defineProperty(juan, "pruebaNasa", {
+//   value: "extraterrestres",
+//   enumerable: false,
+//   writable: false,
+//   configurable: false,
+// });
+
+Object.seal(juan);
+// Object.freeze(juan);
+
+console.log(Object.getOwnPropertyDescriptors(juan));
